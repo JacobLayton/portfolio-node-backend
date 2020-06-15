9167489095684fbc2e6ba7d89ac38cf7c2668d23
JacobLayton <jlayjlay3@gmail.com>
2020-06-14 22:52:20 -0700

    Email post route successfully sends

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Server is running...");
+});
+
+app.post("/email", (req, res) => {
+  console.log("Data: ", req.body);
+  res.json({ message: "Message recieved!" });
 });
 
 app.listen(5000, () => console.log("Server is running on port 5000."));
