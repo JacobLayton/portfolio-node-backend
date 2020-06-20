7a489556d8b3ed46def50a7c5e14befe1db83def
JacobLayton <jlayjlay3@gmail.com>
2020-06-19 18:22:52 -0700

    Changed Port to allow heroku env variables

--- index.js
+++ index.js
@@ -31,4 +31,6 @@ app.post("/email", (req, res) => {
   });
 });
 
-app.listen(5000, () => console.log("Server is running on port 5000."));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
